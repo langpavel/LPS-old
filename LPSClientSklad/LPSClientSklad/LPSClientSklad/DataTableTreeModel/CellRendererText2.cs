50c9cc9056944ed65b18505d471ce035e8738844
Pavel Lang <plang@lang-ubuntu.(none)>
2010-01-19 06:42:47 +0100

    comit

---
+++ LPSClientSklad/LPSClientSklad/LPSClientSklad/DataTableTreeModel/CellRendererText2.cs
@@ -0,0 +1,20 @@
+using System;
+using Gtk;
+using Gdk;
+
+namespace LPSClientSklad
+{
+	public class CellRendererText2 : CellRendererText
+	{
+		public CellRendererText2 ()
+		{
+		}
+
+		public override void GetSize (Widget widget, ref Rectangle cell_area, out int x_offset, out int y_offset, out int width, out int height)
+		{
+			base.GetSize (widget, ref cell_area, out x_offset, out y_offset, out width, out height);
+			y_offset -= 3;
+			height -= 5;
+		}
+	}
+}
